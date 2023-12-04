4fdf1462f7774654750d8bf0532f24da03c937be
Mirjavkharbek <uzmir6358.korea@gmail.com>
2023-12-04 19:11:34 +0900

    main function integrated with traffic function

--- myMain.py
+++ myMain.py
@@ -7,6 +7,10 @@ from object_detection_function_everyNth import detection
 from conversation.listener import listener
 from container import FixedSizeStack
 
+import time
+from multiprocessing import Process, Value
+import RPi.GPIO as GPIO
+
 # Create a Queue instance
 data_queue = FixedSizeStack(5)
 
@@ -18,3 +22,110 @@ listen_thread = threading.Thread(target=listener, args=(data_queue,))
 
 obj_detect_thread.start()
 listen_thread.start()
+
+
+class TrafficLightController:
+    def __init__(self):
+        # Define GPIO pins for red, yellow, and green LEDs
+        self.RED_PIN = 17
+        self.YELLOW_PIN = 18
+        self.GREEN_PIN = 19
+
+        # Set up GPIO mode and pins
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.RED_PIN, GPIO.OUT)
+        GPIO.setup(self.YELLOW_PIN, GPIO.OUT)
+        GPIO.setup(self.GREEN_PIN, GPIO.OUT)
+
+        # Shared variables
+        self.current_light = Value('i', 0)
+        self.priority = Value('i', 0)
+        self.average_priority_point = Value('f', 5.0)  # Using 'f' for float
+
+    def change_light(self, color):
+        GPIO.output(self.RED_PIN, GPIO.LOW)
+        GPIO.output(self.YELLOW_PIN, GPIO.LOW)
+        GPIO.output(self.GREEN_PIN, GPIO.LOW)
+        
+        if color == 0:  # Green
+            GPIO.output(self.GREEN_PIN, GPIO.HIGH)
+        elif color == 1:  # Yellow
+            GPIO.output(self.YELLOW_PIN, GPIO.HIGH)
+        elif color == 2:  # Red
+            GPIO.output(self.RED_PIN, GPIO.HIGH)
+        else:
+            print("Invalid color parameter")
+
+    def get_new_color(self, nb_car):
+        if nb_car and self.average_priority_point.value:
+            if nb_car / self.average_priority_point.value >= 1.75:
+                return 2  # Priority to cars (red)
+            if nb_car / self.average_priority_point.value <= 0.25:
+                return 1  # Priority to people (yellow)
+        return 0  # No priority (green)
+
+    def send_light_color_instruction(self):
+        priority = self.priority.value
+        current_light = self.current_light.value
+
+        if priority == 1 and current_light == 0:
+            time.sleep(6)
+            self.change_light(1)
+            self.current_light.value = 1
+            time.sleep(3)
+            self.change_light(2)
+            self.current_light.value = 2
+            time.sleep(3)
+            self.change_light(2)  # Assuming 2 for red
+            self.current_light.value = 2
+        elif priority == 2 and current_light == 2:
+            time.sleep(2)
+            self.change_light(1)
+            self.current_light.value = 1
+
+    def change_priority_point(self, new_priority):
+        self.average_priority_point.value = new_priority
+
+def simulate_traffic(controller):
+    # Simulating changes in the number of cars
+    while True:
+        # This is just an example; replace this with your actual car count logic
+        time.sleep(5)
+        cars_count = data_queue.get()  # Replace this with your car count logic
+        new_color = controller.get_new_color(cars_count)
+        controller.change_light(new_color)
+
+if __name__ == "__main__":
+    controller = TrafficLightController()
+
+
+
+
+    # Create a multiprocessing queue
+    data_queue = FixedSizeStack(maxsize=5)
+
+    # Start object detection process
+    obj_detect_process = Process(target=detection, args=(data_queue, "1.mp4"))
+    obj_detect_process.start()
+
+    # Start listener process
+    listen_process = Process(target=listener, args=(data_queue,))
+    listen_process.start()
+
+    # Start traffic light control process
+    traffic_process = Process(target=simulate_traffic, args=(controller,))
+    traffic_process.start()
+
+    # Join processes (terminate them when the main process exits)
+    obj_detect_process.join()
+    listen_process.join()
+    traffic_process.join()
+
+    # Modify priority remotely (for demonstration purposes)
+    controller.change_priority_point(10.0)
+    time.sleep(20)  # Let the simulation run for a while before changing priority
+    controller.change_priority_point(6.0)
+    
+    # Clean up GPIO before exiting
+    GPIO.cleanup()
+    
--- smartLightFct.py
+++ smartLightFct.py
@@ -1,47 +1,94 @@
 import time
-from multiprocessing import Process
+from multiprocessing import Process, Value
+import RPi.GPIO as GPIO
 
-current_light = 0
-priority = 0
-avarage_priority_point = 5
+class TrafficLightController:
+    def __init__(self):
+        # Define GPIO pins for red, yellow, and green LEDs
+        self.RED_PIN = 17
+        self.YELLOW_PIN = 18
+        self.GREEN_PIN = 19
 
-def get_new_color(nb_car):
-    """
-    This function creates a new color for the light depending on the number of people and cars
-    
-    :param nb_people: number of people
-    :param nb_car: number of cars
-    :return: a tuple containing the new color for the light, if 0 then no priority, if 1 then priority to people, if 2 then priority to cars
-    """
-    if nb_car and avarage_priority_point:
-        if nb_car/avarage_priority_point >= 1.75:
-            return 2
-        if nb_car/avarage_priority_point <= 0.25:
-            return 1
-    return 0
+        # Set up GPIO mode and pins
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.RED_PIN, GPIO.OUT)
+        GPIO.setup(self.YELLOW_PIN, GPIO.OUT)
+        GPIO.setup(self.GREEN_PIN, GPIO.OUT)
 
-def send_light_color_instruction(priority, current_light=current_light):
-    """
-    Based on the priority it will change the color of the light
+        # Shared variables
+        self.current_light = Value('i', 0)
+        self.priority = Value('i', 0)
+        self.average_priority_point = Value('f', 5.0)  # Using 'f' for float
 
-    :param priority: the priority of the light
-    
-    :param current_light: the current light (0 for green, 1 for yellow, 2 for red)
-    """
-    if priority == 0:
-        None
-    if priority == 1 and current_light == 0:
-        time.sleep(6)
-        current_light = 1
-        time.sleep(3)
-        current_light = 2
-        time.sleep(3)
-        current_light = 3
-    elif priority == 2 and current_light == 2:
-        time.sleep(2)
-        current_light = 1
+    def change_light(self, color):
+        GPIO.output(self.RED_PIN, GPIO.LOW)
+        GPIO.output(self.YELLOW_PIN, GPIO.LOW)
+        GPIO.output(self.GREEN_PIN, GPIO.LOW)
+        
+        if color == 0:  # Green
+            GPIO.output(self.GREEN_PIN, GPIO.HIGH)
+        elif color == 1:  # Yellow
+            GPIO.output(self.YELLOW_PIN, GPIO.HIGH)
+        elif color == 2:  # Red
+            GPIO.output(self.RED_PIN, GPIO.HIGH)
+        else:
+            print("Invalid color parameter")
 
-"""Function in case if user want to change priority point remotely"""
-def change_priority_point(new_priority):
-    avarage_priority_point = new_priority
-    
+    def get_new_color(self, nb_car):
+        if nb_car and self.average_priority_point.value:
+            if nb_car / self.average_priority_point.value >= 1.75:
+                return 2  # Priority to cars (red)
+            if nb_car / self.average_priority_point.value <= 0.25:
+                return 1  # Priority to people (yellow)
+        return 0  # No priority (green)
+
+    def send_light_color_instruction(self):
+        priority = self.priority.value
+        current_light = self.current_light.value
+
+        if priority == 1 and current_light == 0:
+            time.sleep(6)
+            self.change_light(1)
+            self.current_light.value = 1
+            time.sleep(3)
+            self.change_light(2)
+            self.current_light.value = 2
+            time.sleep(3)
+            self.change_light(2)  # Assuming 2 for red
+            self.current_light.value = 2
+        elif priority == 2 and current_light == 2:
+            time.sleep(2)
+            self.change_light(1)
+            self.current_light.value = 1
+
+    def change_priority_point(self, new_priority):
+        self.average_priority_point.value = new_priority
+
+
+def simulate_traffic(controller):
+    # Simulating changes in the number of cars
+    while True:
+        # This is just an example; replace this with your actual car count logic
+        time.sleep(5)
+        cars_count = 15  # Replace this with your car count logic
+        new_color = controller.get_new_color(cars_count)
+        controller.change_light(new_color)
+
+
+if __name__ == '__main__':
+    controller = TrafficLightController()
+
+    # Create a process for simulating traffic
+    traffic_process = Process(target=simulate_traffic, args=(controller,))
+    traffic_process.start()
+
+    # Modify priority remotely (for demonstration purposes)
+    time.sleep(20)  # Let the simulation run for a while before changing priority
+    controller.change_priority_point(6.0)
+
+    # Terminate the simulation after a while (for demonstration purposes)
+    time.sleep(40)
+    traffic_process.terminate()
+
+    # Clean up GPIO before exiting
+    GPIO.cleanup()
